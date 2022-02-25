905e01d1fba1daee4d3f7c6d95ff9a7acb57f096
Phuong Vu <tariusagi@gmail.com>
2022-02-25 15:06:02 +0700

    - Added read only mode to imgmount and improved text output. - Fixed and improved ansilib.

--- lib/ansilib.sh
+++ lib/ansilib.sh
@@ -12,40 +12,55 @@
 #   red $(yellow_bg A red text on yellow background)
 # Output of these functions can be used for other commands just as normal. For example:
 #   echo This line has a $(red $(yellow_bg red text on yellow background)) inside it
+# NOTE: to preserve whitespaces, double-quote all texts and nested outputs, example:
+#   echo "This  line  has $(red "$(yellow_bg "red   text   on   yellow   background")") inside it"
+
+ANSI_FOREGROUNDS='black red green yellow blue purple cyan white hblack hred hgreen hyellow hblue hpurple hcyan hwhite ublack ured ugreen uyellow ublue upurple ucyan uwhite'
+ANSI_BACKGROUNDS='black red green yellow blue purple cyan white'
 
 # Foreground.
-black() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;30m'$s || echo -e '\033[0;31m'$s'\033[0m'; }
-red() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;31m'$s || echo -e '\033[0;31m'$s'\033[0m'; }
-green() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;32m'$s || echo -e '\033[0;32m'$s'\033[0m'; }
-yellow() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;33m'$s || echo -e '\033[0;33m'$s'\033[0m'; }
-blue() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;34m'$s || echo -e '\033[0;34m'$s'\033[0m'; }
-purple() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;35m'$s || echo -e '\033[0;35m'$s'\033[0m'; }
-cyan() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;36m'$s || echo -e '\033[0;36m'$s'\033[0m'; }
-white() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;37m'$s || echo -e '\033[0;37m'$s'\033[0m'; }
+black() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;30m'"$*" || echo -en '\033[0;30m'"$*"'\033[0m'; }
+red() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;31m'"$*" || echo -en '\033[0;31m'"$*"'\033[0m'; }
+green() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;32m'"$*" || echo -en '\033[0;32m'"$*"'\033[0m'; }
+yellow() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;33m'"$*" || echo -en '\033[0;33m'"$*"'\033[0m'; }
+blue() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;34m'"$*" || echo -en '\033[0;34m'"$*"'\033[0m'; }
+purple() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;35m'"$*" || echo -en '\033[0;35m'"$*"'\033[0m'; }
+cyan() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;36m'"$*" || echo -en '\033[0;36m'"$*"'\033[0m'; }
+white() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;37m'"$*" || echo -en '\033[0;37m'"$*"'\033[0m'; }
 # Hi-intensive foreground.
-hblack() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;90m'$s || echo -e '\033[0;90m'$s'\033[0m'; }
-hred() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;91m'$s || echo -e '\033[0;91m'$s'\033[0m'; }
-hgreen() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;92m'$s || echo -e '\033[0;92m'$s'\033[0m'; }
-hyellow() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;93m'$s || echo -e '\033[0;93m'$s'\033[0m'; }
-hblue() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;94m'$s || echo -e '\033[0;94m'$s'\033[0m'; }
-hpurple() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;95m'$s || echo -e '\033[0;95m'$s'\033[0m'; }
-hcyan() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;96m'$s || echo -e '\033[0;96m'$s'\033[0m'; }
-hwhite() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[0;97m'$s || echo -e '\033[0;97m'$s'\033[0m'; }
+hblack() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;90m'"$*" || echo -en '\033[0;90m'"$*"'\033[0m'; }
+hred() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;91m'"$*" || echo -en '\033[0;91m'"$*"'\033[0m'; }
+hgreen() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;92m'"$*" || echo -en '\033[0;92m'"$*"'\033[0m'; }
+hyellow() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;93m'"$*" || echo -en '\033[0;93m'"$*"'\033[0m'; }
+hblue() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;94m'"$*" || echo -en '\033[0;94m'"$*"'\033[0m'; }
+hpurple() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;95m'"$*" || echo -en '\033[0;95m'"$*"'\033[0m'; }
+hcyan() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;96m'"$*" || echo -en '\033[0;96m'"$*"'\033[0m'; }
+hwhite() { [ "${*: -3}" == "[0m" ] && echo -en '\033[0;97m'"$*" || echo -en '\033[0;97m'"$*"'\033[0m'; }
 # Underlined foregrround.
-ublack() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;30m'$s || echo -e '\033[4;30m'$s'\033[0m'; }
-ured() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;31m'$s || echo -e '\033[4;31m'$s'\033[0m'; }
-ugreen() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;32m'$s || echo -e '\033[4;32m'$s'\033[0m'; }
-uyellow() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;33m'$s || echo -e '\033[4;33m'$s'\033[0m'; }
-ublue() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;34m'$s || echo -e '\033[4;34m'$s'\033[0m'; }
-upurple() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;35m'$s || echo -e '\033[4;35m'$s'\033[0m'; }
-ucyan() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;36m'$s || echo -e '\033[4;36m'$s'\033[0m'; }
-uwhite() { s="$*"; [ "${s: -3}" == "[0m" ] && echo -e '\033[4;37m'$s || echo -e '\033[4;37m'$s'\033[0m'; }
+ublack() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;30m'"$*" || echo -en '\033[4;30m'"$*"'\033[0m'; }
+ured() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;31m'"$*" || echo -en '\033[4;31m'"$*"'\033[0m'; }
+ugreen() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;32m'"$*" || echo -en '\033[4;32m'"$*"'\033[0m'; }
+uyellow() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;33m'"$*" || echo -en '\033[4;33m'"$*"'\033[0m'; }
+ublue() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;34m'"$*" || echo -en '\033[4;34m'"$*"'\033[0m'; }
+upurple() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;35m'"$*" || echo -en '\033[4;35m'"$*"'\033[0m'; }
+ucyan() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;36m'"$*" || echo -en '\033[4;36m'"$*"'\033[0m'; }
+uwhite() { [ "${*: -3}" == "[0m" ] && echo -en '\033[4;37m'"$*" || echo -en '\033[4;37m'"$*"'\033[0m'; }
 # Background.
-black_bg() { echo -e '\033[40m'$*'\033[0m'; }
-red_bg() { echo -e '\033[41m'$*'\033[0m'; }
-green_bg() { echo -e '\033[42m'$*'\033[0m'; }
-yellow_bg() { echo -e '\033[43m'$*'\033[0m'; }
-blue_bg() { echo -e '\033[44m'$*'\033[0m'; }
-purple_bg() { echo -e '\033[45m'$*'\033[0m'; }
-cyan_bg() { echo -e '\033[46m'$*'\033[0m'; }
-white_bg() { echo -e '\033[47m'$*'\033[0m'; }
+black_bg() { echo -en '\033[40m'"$*"'\033[0m'; }
+red_bg() { echo -en '\033[41m'"$*"'\033[0m'; }
+green_bg() { echo -en '\033[42m'"$*"'\033[0m'; }
+yellow_bg() { echo -en '\033[43m'"$*"'\033[0m'; }
+blue_bg() { echo -en '\033[44m'"$*"'\033[0m'; }
+purple_bg() { echo -en '\033[45m'"$*"'\033[0m'; }
+cyan_bg() { echo -en '\033[46m'"$*"'\033[0m'; }
+white_bg() { echo -en '\033[47m'"$*"'\033[0m'; }
+
+ansi_colors_table(){
+	# Output a table of ANSI colors combinations.
+	for f in $ANSI_FOREGROUNDS; do
+		for b in $ANSI_BACKGROUNDS; do
+			$f "$(${b}_bg "$f text on $b background")"
+		done
+	done
+}
+
